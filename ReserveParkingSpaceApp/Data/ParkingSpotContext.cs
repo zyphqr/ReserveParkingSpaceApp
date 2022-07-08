242255f7508bb266be4530d50b3b98861c77a81d
User <User@DESKTOP-PC>
2022-07-08 11:20:30 +0300

    Add project files.

---
+++ ReserveParkingSpaceApp/Data/ParkingSpotContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReserveParkingSpaceApp.Models;
+
+namespace ReserveParkingSpaceApp.Data
+{
+    public class ParkingSpotContext : DbContext
+    {
+        public ParkingSpotContext(DbContextOptions<ParkingSpotContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ParkingSpot> ParkingSpots { get; set; }
+    }
+}
---
+++ ReserveParkingSpaceApp/Models/ParkingSpot.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveParkingSpaceApp.Models
+{
+    public class ParkingSpot
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsTaken { get; set; }
+        public string TakenBy { get; set; }
+    }
+}
